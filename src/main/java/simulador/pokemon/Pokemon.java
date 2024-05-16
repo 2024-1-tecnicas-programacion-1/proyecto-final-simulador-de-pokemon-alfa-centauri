85c3f461a4e046074cdea2bd96071cbb1fa5da10
user <user@email.com>
2024-05-16 10:30:51 -0500

    Avance Tipo pokemon

--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -1,5 +1,23 @@
 package simulador.pokemon;
 
-public class Pokemon {
-    // TODO: Aquí va tu código
+public abstract class Pokemon {
+    private String nombre;
+    private int salud;
+    private int pubtosDeAtaque;
+    private String tipo;
+    private String estado;
+
+    public Pokemon(String nombre, int salud, int pubtosDeAtaque, String tipo, String estado) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.pubtosDeAtaque = pubtosDeAtaque;
+        this.tipo = tipo;
+        this.estado = estado;
+    }
+    public abstract void atacar();
+    
+    public abstract int recibirDanio(int danio);
+    
+    public abstract void entrenar();
+    
 }
--- src/main/java/simulador/pokemon/TipoPokemon.java
+++ src/main/java/simulador/pokemon/TipoPokemon.java
@@ -1,5 +1,23 @@
 package simulador.pokemon;
 
-public class TipoPokemon {
-    // TODO: Aquí va tu código
+public enum TipoPokemon{
+
+    FUEGO, AGUA, PLANTA, VENENO, ELECTRICO, PSIQUICO, ROCA, TIERRA, NORMAL, VOLADOR, HADA, LUCHA, ACERO, BICHO, HIELO, FANTASMA,
+    
+    public static double obtenerMultiplicadorDeDanio(TipoPokemon atacante, TipoPokemon defensor){
+        
+    }
+    
+    public void atacar() {
+
+    }
+
+    public int recibirDanio(int danio) {
+
+    }
+
+    public void entrenar() {
+
+    }
+
 }
